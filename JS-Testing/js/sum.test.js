c223adc46aa83a9acf4ce3e6909f60031c01de61
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-05-01 21:21:40 +0430

    matchers

--- JS-Testing/js/sum.test.js
+++ JS-Testing/js/sum.test.js
@@ -3,3 +3,21 @@ const sum = require("./sum")
 test("properly adds two numbers", () => {
     expect(sum(1, 2)).toBe(3)
 })
+
+/////////////////
+/// Matchers ///
+////////////////
+
+// null
+test("null is falsy", () => {
+    const n = null;
+    expect(n).toBeFalsy()
+})
+
+// zero
+test("null is falsy", () => {
+    const n = 0;
+    expect(n).toBeFalsy()
+})
+
+// 
